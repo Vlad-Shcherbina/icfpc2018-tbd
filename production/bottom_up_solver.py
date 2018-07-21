78c47c1860c3056e257a7e78ce5f1e435057ea98
Kirill Elagin <kirelagin@gmail.com>
2018-07-21 16:12:39 +0300

    wip

---
+++ production/bottom_up_solver.py
@@ -0,0 +1,49 @@
+import logging
+logger = logging.getLogger(__name__)
+
+from production.model import Model
+from production.commands import *
+from production.basics import Pos, Diff
+from production.solver_utils import *
+import sys
+
+def up_pass(model):
+    steps = []
+
+    # Up 1 and forward 1
+    steps.append([LMove(Diff(0,1,0), Diff(0,0,1))])
+
+    # Distribute
+    (steps_distr, strips) = fission_fill_right(list(range(2, 21)), model.R)
+    steps.extend(steps_distr)
+
+    for layer in range(0, model.R - 1):
+        steps.extend(print_layer_below(model, layer, strips))
+        steps.extend([[SMove(Diff(0,1,0))]] * len(strips))
+
+    return steps
+
+
+def write_solution(bytetrace, number): # -> IO ()
+    with open('problemsL/LA{0:03d}.nbt'.format(number), 'wb') as f:
+        f.write(bytetrace)
+
+def solve(strategy, model, number = 0): # -> IO ()
+    commands = [cmd for step in strategy(model) for cmd in step]
+    trace = compose_commands(commands)
+    write_solution(trace, number)
+
+def main():
+    from production import data_files
+
+    task_number = int(sys.argv[1]) if len(sys.argv) > 1 else 1
+
+    name = 'LA{0:03d}_tgt.mdl'.format(task_number)
+    data = data_files.lightning_problem(name)
+    m = Model.parse(data)
+
+    solve(up_pass, m, task_number)
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG, format='%(levelname).1s %(asctime)s %(module)10.10s:%(lineno)-4d %(message)s')
+    main()
--- production/solver_utils_test.py
+++ production/solver_utils_test.py
@@ -56,3 +56,35 @@ def test_bounding_box_region():
                 m[Pos(x, y, z)] = bool(cell)
 
     assert bounding_box_region(m, fx = 2) == (Pos(2, 1, 0), Pos(2, 1, 1))
+
+
+def test_projection_top():
+    matrix = [
+        #   z
+        #  ---->
+        [[1, 0, 0],   # |
+         [0, 1, 0],   # | y
+         [0, 0, 0]],  # v
+        # x = 0
+
+        [[0, 0, 0],
+         [0, 1, 0],
+         [0, 0, 0]],
+        # x = 1
+
+        [[0, 0, 0],
+         [1, 1, 0],
+         [0, 0, 0]],
+        # x = 2
+    ]
+    m = Model(3)
+    for x, slice in enumerate(matrix):
+        for y, row in enumerate(slice):
+            for z, cell in enumerate(row):
+                m[Pos(x, y, z)] = bool(cell)
+
+    assert projection_top(m) == [
+        [1, 1, 0],
+        [0, 1, 0],
+        [1, 1, 0]
+    ]
