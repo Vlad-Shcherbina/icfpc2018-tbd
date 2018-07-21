9f056a561987cef7758da88f29ae1c2a1a984734
caudate_julie <kyra-mensk@yandex.ru>
2018-07-21 12:21:30 +0300

    cpp emulator: set of commands

--- production/cpp_emulator/emulator.cpp
+++ production/cpp_emulator/emulator.cpp
@@ -9,6 +9,13 @@
 
 using std::vector;
 using std::string;
+using std::unique_ptr;
+using std::make_unique;
+
+
+// ENERGY CONSTANTS
+
+
 
 
 /*======================== DIFF =========================*/
@@ -94,6 +101,306 @@ int region_dimension(const Pos& a, const Pos& b) {
 }
 
 
+/*====================== COMMANDS =======================*/
+
+class Bot;
+class Emulator;
+
+struct Command {
+	virtual ~Command() = default;
+	virtual void execute(Bot*, Emulator*) = 0;
+	virtual void set_volatiles(Bot*, Emulator*) = 0;
+	static unique_ptr<Command> getnextcommand(Emulator* field);	// defined after Emulator
+
+	static Diff get_nd(unsigned char byte) {
+		byte >>= 3;
+		int dz = byte % 3 - 1;
+		byte /= 3;
+		int dy = byte % 3 - 1;
+		byte /= 3;
+		int dx = byte % 3 - 1;
+		return Diff(dx, dy, dz);
+	}
+
+	static Diff get_lld(unsigned char a, unsigned char i) {
+		a &= 3;
+		int d = (int)(i &= 31) - 15;
+		if (a == 1) return Diff(d, 0, 0);
+		if (a == 2) return Diff(0, d, 0);
+		if (a == 3) return Diff(0, 0, d);
+		// TODO: report ill-formed model
+	}
+
+	static Diff get_sld(unsigned char a, unsigned char i) {
+		a &= 3;
+		int d = (int)(i &= 15) - 5;
+		if (a == 1) return Diff(d, 0, 0);
+		if (a == 2) return Diff(0, d, 0);
+		if (a == 3) return Diff(0, 0, d);
+		// TODO: report ill-formed model
+	}
+
+};
+
+struct Halt : Command {
+	void execute(Bot* b, Emulator* f) {
+		// TODO
+	}
+
+	void set_volatiles(Bot* b, Emulator* f) {
+	}
+};
+
+struct Wait : Command {
+	void execute(Bot* b, Emulator* f) {}
+
+	void set_volatiles(Bot* b, Emulator* f) {
+	}
+};
+
+struct Flip : Command {
+	void execute(Bot* b, Emulator* f) {
+	}
+
+	void set_volatiles(Bot* b, Emulator* f) {
+	}
+};
+
+struct SMove : Command {
+	Diff lld;
+
+	SMove(Diff d)
+	: lld(d)
+	{
+		// TODO: report wrong command
+		//assert (lld.is_long());
+	}
+
+	void execute(Bot* b, Emulator* f) {
+	}
+
+	void set_volatiles(Bot* b, Emulator* f) {
+	}
+};
+
+struct LMove : Command {
+	Diff sld1, sld2;
+	LMove(Diff d1, Diff d2)
+	: sld1(d1)
+	, sld2(d2)
+	{
+		// TODO: report wrong command
+		// assert (sld1.is_short() && sld2.is_short());
+	}
+
+	void execute(Bot* b, Emulator* f) {
+	}
+
+	void set_volatiles(Bot* b, Emulator* f) {
+	}
+};
+
+struct FusionP : Command {
+	Diff nd;
+
+	FusionP(Diff nd)
+	: nd(nd)
+	{
+		// TODO: report wrong command
+		//assert(nd.is_near());
+	}
+
+	void execute(Bot* b, Emulator* f) {
+	}
+
+	void set_volatiles(Bot* b, Emulator* f) {
+	}
+};
+
+struct FusionS : Command
+{
+	Diff nd;
+
+	FusionS(Diff nd)
+	: nd(nd)
+	{
+		// TODO: report wrong command
+		//assert(nd.is_near());
+	}
+
+	void execute(Bot* b, Emulator* f) {
+	}
+
+	void set_volatiles(Bot* b, Emulator* f) {
+	}
+};
+
+struct Fission : Command
+{
+	Diff nd;
+	unsigned short m;
+	Fission(Diff nd, unsigned short m)
+	: nd(nd)
+	, m(m)
+	{
+		// TODO: report wrong command
+		//assert(nd.is_near());
+	}
+
+	void execute(Bot* b, Emulator* f) {
+	}
+
+	void set_volatiles(Bot* b, Emulator* f) {
+	}
+};
+
+struct Fill : Command
+{	Diff nd;
+
+	Fill(Diff nd)
+	: nd(nd)
+	{
+		// TODO: report wrong command
+		//assert(nd.is_near());
+	}
+
+	void execute(Bot* b, Emulator* f) {
+	}
+
+	void set_volatiles(Bot* b, Emulator* f) {
+	}
+};
+
+/*======================== BOT ==========================*/
+
+class Bot {
+public:
+	int pid;
+	Pos position;
+	vector<int> seeds;
+
+	Bot(int pid, Pos position, vector<int> seeds)
+	: pid(pid)
+	, position(position)
+	, seeds(seeds)
+	{ }
+
+	Bot()
+	: pid(1)
+	, position(Pos(0, 0, 0)) {
+		for (int i = 2; i <= 20; i++) this-> seeds.push_back(i);
+	}
+};
+
+
+/*====================== EMULATOR =======================*/
+
+class Emulator {
+public:
+	int energy;
+	bool high_harmonics;
+	vector<unsigned char> matrix;
+	int R;
+	vector<Bot> bots;
+
+	vector<unsigned char> trace;
+	int tracepointer;
+	int time_step;
+
+	vector<unsigned char> floating;
+	vector<unsigned char> model;
+	vector<unsigned char> volatiles;
+	bool volatile_violation;
+
+	Emulator() {
+		energy = 0;
+		high_harmonics = false;
+		R = 0;
+		bots.push_back(Bot());
+		tracepointer = 0;
+		volatile_violation = false;
+		time_step = 0;
+	}
+
+	~Emulator() {
+
+	}
+
+
+	void read_model(string filename) {
+		std::ifstream f(filename, std::ios::binary);
+		unsigned char c;
+		f >> c;
+		R = (int)c;
+		while (f >> c) model.push_back(c);
+		f.close();
+
+		int bytes = (R * R * R + 7) / 8;
+		matrix = vector<unsigned char>(bytes, 0);
+		floating = vector<unsigned char>(bytes, 0);
+		volatiles = vector<unsigned char>(bytes, 0);
+	}
+
+	void read_trace(string filename) {
+		std::ifstream f(filename, std::ios::binary);
+		unsigned char c;
+		while (f >> c) {
+			trace.push_back(c);
+		}
+		f.close();
+	}
+
+	unsigned char tracebyte() {
+		return trace[tracepointer++];
+	}
+
+	bool getbit(const Pos& p, const vector<unsigned char>& v) const {
+		int w = p.x*R*R + p.y*R + p.z;
+		// TODO: TEST!
+		return v[w / 8] & (1 << (w % 8));
+	}
+
+	void setbit(const Pos& p, vector<unsigned char>& v, bool bit) {
+		int w = p.x*R*R + p.y*R + p.z;
+		// TODO: TEST!
+		v[w / 8] ^= (getbit(p, v) == bit) << (w % 8);
+	}
+
+
+	void run(string modelfile, string tracefile) {
+		// test run
+		read_model(modelfile);
+		read_trace(tracefile);
+		for (int i = 0; i < 20; i++) {
+			unique_ptr<Command> c = Command::getnextcommand(this);
+		}
+	}
+};
+
+
+/*===================== FUNCTIONS =======================*/
+
+unique_ptr<Command> Command::getnextcommand(Emulator* field) {
+	unsigned char byte = field->tracebyte();
+	if (byte == 255) return make_unique<Halt>(Halt());
+	if (byte == 254) return make_unique<Wait>(Wait());
+	if (byte == 253) return make_unique<Flip>(Flip());
+	unsigned char tail = byte & 7;
+	if (tail == 3) return make_unique<Fill>(Fill(get_nd(byte)));
+	if (tail == 6) return make_unique<FusionS>(FusionS(get_nd(byte)));
+	if (tail == 7) return make_unique<FusionP>(FusionP(get_nd(byte)));
+
+	unsigned char byte2 = field->tracebyte();
+	if (tail == 5) return make_unique<Fission>(Fission(get_nd(byte), byte2));
+	if (tail == 4) {
+		if (byte & 8) return make_unique<LMove>(LMove(get_sld(byte>>6, byte2>>4),
+											   		  get_sld(byte>>4, byte2)));
+		else		  return make_unique<SMove>(SMove(get_lld(byte>>4, byte2)));
+	}
+	// TODO: report ill-formed model
+	return unique_ptr<Command>(nullptr);
+}
+
 /*====================== BINDING ========================*/
 
 namespace py = pybind11;
@@ -126,6 +433,12 @@ PYBIND11_MODULE(emulator, m) {
 		.def(py::self == py::self)
 		.def(py::self != py::self)
 		.def("__str__", &Pos::__str__)
+	;
+
+	py::class_<Emulator> EmClass(m, "Emulator");
+	EmClass
+		.def(py::init<>())
+		.def("run", &Emulator::run)
 	;
 
 	m.def("region_dimension", &region_dimension);
