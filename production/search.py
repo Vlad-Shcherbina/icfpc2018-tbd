d3f8584e730ef56dd0cb829ab99284ce89af6819
Blake Elias <blakeelias@gmail.com>
2018-07-22 23:47:22 -0400

    Fix errors in breadth first search implementation

--- production/search.py
+++ production/search.py
@@ -21,19 +21,42 @@ def depth_first_search(graph: Iterable[T], valid_neighbors: Callable[[T], Iterab
 
     return order
 
+def breadth_first_search(roots, valid_neighbors):
+    # a FIFO open_set
+    open_set = []
+    
+    # an empty set to maintain visited nodes
+    closed_set = set()
 
+    # an empty list to store the order in which nodes are visited
+    traversal = []
+    
+    # initialize
+    for root in roots:
+        open_set.append(root)
 
-def breadth_first_search(graph: Iterable[T], valid_neighbors: Callable[[T], Iterable[T]]) -> List[T]:
-    g = iter(graph)
-    try:
-        first = g.__next__()
-    except StopIteration:
-        return graph
+    # For each node on the current level expand and process, if no children 
+    # (leaf) then unwind
+    while len(open_set) > 0:
 
-    return chain(
-        iter([first]),
-        breadth_first_search(
-            chain(
-                g,
-                valid_neighbors(first)),
-            valid_neighbors))
+        # get first item from queue
+        subtree_root = open_set.pop(0)
+    
+        # For each child of the current tree process
+        for child in valid_neighbors(subtree_root):
+      
+            # The node has already been processed, so skip over it
+            if child in closed_set:
+                continue
+      
+            # The child is not enqueued to be processed, so enqueue this level of
+            # children to be expanded
+            if child not in open_set:
+                open_set.append(child)              # enqueue these nodes
+    
+        # We finished processing the root of this subtree, so add it to the closed 
+        # set
+        closed_set.add(subtree_root)
+        traversal.append(subtree_root)
+
+    return traversal
